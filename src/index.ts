8ea491293fcad908148732ac04c30782a76f19c4
Danielle Church <dchurch@akamai.com>
2019-10-01 15:23:52 -0400

    Perform functional checks when deciding whether to polyfill

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,29 @@
 
 import Module from 'module';
 import semver from 'semver';
+import path from 'path';
 import { getPath, poly_resolveLookupPaths, poly_resolveFilename, createRequire, createRequireFromPath } from './node-internals/module';
+import { URL } from 'url';
 
 function hasArgument(func:Function, argName:string):boolean {
     const argmatch = /^function[^(]*\(([^)]*)\)/.exec(func.toString());
     if (!argmatch) return false;
     return argmatch[1].split(/,\s*/).includes(argName);
+}
+
+function requireTest(createRequire:((arg:any)=>(id:string)=>any)|undefined, requireArg:any,...moduleIds:string[]):boolean {
+    if (!createRequire) {
+        return false;
+    }
+    try {
+        const requireFunc = createRequire(requireArg);
+        for (const moduleId of moduleIds) {
+            requireFunc(moduleId);
+        }
+    } catch (e) {
+        return false;
+    }
+    return true;
 }
 
 if (semver.gte(process.versions.node, "12.2.0")) {
@@ -31,13 +48,17 @@ if (semver.gte(process.versions.node, "12.2.0")) {
         Module._resolveFilename = poly_resolveFilename(Module._resolveFilename);
     }
 
-    if (!('createRequire' in (Module as any))) {
+    if (!requireTest(Module.createRequireFromPath, __filename, "./node-internals/module") ||
+        !requireTest(Module.createRequireFromPath, path.join(__dirname,path.sep), "./node-internals/module")) {
+        console.debug("overriding Module.createRequireFromPath()");
+        Module.createRequireFromPath = createRequireFromPath;
+    }
+
+    if (!requireTest(Module.createRequire, __filename, "./node-internals/module") ||
+        !requireTest(Module.createRequire, path.join(__dirname,path.sep), "./node-internals/module") ||
+        !requireTest(Module.createRequire, new URL(`file://${__filename}`), "./node-internals/module")) {
         console.debug("overriding Module.createRequire()");
         Module.createRequire = createRequire;
     }
 
-    if (!('createRequireFromPath' in (Module as any))) {
-        console.debug("overriding Module.createRequireFromPath()");
-        Module.createRequireFromPath = createRequireFromPath;
-    }
 }
